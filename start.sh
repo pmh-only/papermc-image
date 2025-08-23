9f94c1ad2f19cb033b740f9985c636c15a92738a
Minhyeok Park <pmh_only@pmh.codes>
2025-08-23 17:04:59 +0900

    fix(start.sh): invert feature flag condition

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 # from https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/
 TUNING_FLAG="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 
-if [[ -z "$DISABLE_TUNING" ]]; then
+if [[ -n "$DISABLE_TUNING" ]]; then
   TUNING_FLAG=""
 fi
 
-if [[ -z "$ACCEPT_EULA" ]]; then
+if [[ -n "$ACCEPT_EULA" ]]; then
   echo "eula=true" > /app/eula.txt
 fi
 
